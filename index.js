a0be51ee0a3299ee59b539eecd561704d5351477
Shubham Daulkar <daulkarshubham@gmail.com>
2023-12-29 23:40:25 +0530

    server side rendering done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require("path");
 const URL = require("./models/url");
 const {connectToMongoDB} = require("./connect");
 
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,19 @@ const PORT = 8001;
 connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(() => console.log("mongoDB connected"));
 
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+
 
 app.use("/url",urlRoute);
 
-app.get("/:shortId",async (req,res) => {
+app.use("/",staticRoute);
+
+app.get("/url:/shortId",async (req,res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId,
