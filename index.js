e426bd1e26226b3a18ef3c1bd526f4855622f9be
Shubham Daulkar <daulkarshubham@gmail.com>
2024-01-06 23:20:09 +0530

    nodejs authentication done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const URL = require("./models/url");
 const {connectToMongoDB} = require("./connect");
+const {restrictToLoggedinUserOnly,checkAuth} = require("./middleware/auth");
 
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -17,14 +20,15 @@ app.set("views",path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
+app.use(cookieParser());
 
 
 
-app.use("/url",urlRoute);
+app.use("/url",restrictToLoggedinUserOnly,urlRoute);
+app.use("/user",userRoute);
+app.use("/",checkAuth,staticRoute);
 
-app.use("/",staticRoute);
-
-app.get("/url:/shortId",async (req,res) => {
+app.get("/url/:shortId",async (req,res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId,
